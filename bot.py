58d25192601e8f523755d67bb5e5efb9f0849359
turbo Fell <turbofell1934@gmail.com>
2026-06-27 15:52:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
+# bot.py - Simple Working Bot
+import discord
+import os
 
+# Token GitHub Secrets se le rahe hain (Secure!)
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+class MyBot(discord.Client):
+    async def on_ready(self):
+        print(f"Bot Online! {self.user}")
+    
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        
+        # Simple commands
+        if message.content == "!ping":
+            await message.channel.send("Pong!")
+        
+        if message.content == "!hello":
+            await message.channel.send(f"Hello {message.author.name}!")
+
+# Run
+client = MyBot()
+client.run(TOKEN)
